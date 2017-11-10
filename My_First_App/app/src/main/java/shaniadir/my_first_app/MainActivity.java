c9e30bf9700c5871c973251fbd5bf9d497bda52a
Shani Adir <shaniadir@Shanis-MacBook-Pro.local>
2017-11-10 19:15:20 +0200

    move to fragment and center Toast

--- My_First_App/app/src/main/java/shaniadir/my_first_app/MainActivity.java
+++ My_First_App/app/src/main/java/shaniadir/my_first_app/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -41,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 user_text_to_show = user_input.getText().toString();
                 Toast toast = Toast.makeText(view.getContext(), user_text_to_show, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0,0);
                 toast.show();
             }
         });
