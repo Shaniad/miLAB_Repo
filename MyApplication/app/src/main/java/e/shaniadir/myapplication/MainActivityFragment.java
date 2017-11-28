85ccde1e86fb4f6292475628b8f0e8410c185b73
Shani Adir <shaniadir@Shanis-MacBook-Pro.local>
2017-11-29 00:03:26 +0200

    try to get a response

--- MyApplication/app/src/main/java/e/shaniadir/myapplication/MainActivityFragment.java
+++ MyApplication/app/src/main/java/e/shaniadir/myapplication/MainActivityFragment.java
@@ -1,16 +1,21 @@
 package e.shaniadir.myapplication;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -23,17 +28,38 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d("debug", "success0000");
         View main_view = inflater.inflate(R.layout.fragment_main, container, false);
+        final RequestQueue queue = Volley.newRequestQueue(getContext());
+        final String url = "http://www.google.com/search?q=";
         final EditText query = (EditText)main_view.findViewById(R.id.query_editText);
+        final TextView results = (TextView)main_view.findViewById(R.id.search_results_textView);
         Button search = (Button)main_view.findViewById(R.id.search_button);
-        Log.d("start", "success0");
+        Log.d("debug", "success0");
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("debug", "success1");
+                StringRequest req = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("MainActivityFragment", "Response - " + response);
+                        results.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("MainActivityFragment", "Encountered error - " + error);
+                    }
+                /*
                 Log.d("afterclick", "success1");
                 Toast.makeText(getActivity(), query.getText().toString(), Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.co.il/search?q=" + query.getText())));
                 Log.d("search", "success2");
+                */
+                });
+                queue.add(req);
+                Log.d("debug", "success2");
             }
         });
         return main_view;
